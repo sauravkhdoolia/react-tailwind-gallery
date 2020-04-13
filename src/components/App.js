5dbac0398f04f57f700682965b6d053b23ae6ab1
Saurav kumar <sauravkhdoolia@gmail.com>
2020-04-13 12:21:39 +0530

    Removed CRA stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className='App'>ABS</div>;
+  return (
+    <div>
+      <h1 className='text-3xl'>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
